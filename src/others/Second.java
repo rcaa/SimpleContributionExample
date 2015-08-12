13d40845ab8c1dcfc1f08c2fca89dbc1192747be
Rodrigo Andrade <rodrigo_cardoso@hotmail.it>
2015-08-12 09:39:33 -0300

    new example

---
+++ src/others/Second.java
@@ -0,0 +1,19 @@
+package others;
+
+public class Second {
+
+	int x;
+	
+	public void m() {
+		
+	}
+	
+	public void c() {
+		System.out.println(x);
+	}
+	
+	public static void main(String[] args) {
+		Second s = new Second();
+		s.m();
+	}
+}
