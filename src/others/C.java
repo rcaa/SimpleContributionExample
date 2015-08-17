cd05cb8fba6621b64799735da179e6794c810bb3
Rodrigo Andrade <rodrigo_cardoso@hotmail.it>
2015-08-17 11:49:09 -0300

    new example

--- src/others/C.java
+++ src/others/C.java
@@ -3,19 +3,23 @@ package others;
 public class C {
 
 	int x;
-	
-	public void n() {
+
+	public void n(Object obj) {
 		D d = new D();
-		d.m(x);
+		if (obj == null) {
+			d.m(5);
+		} else {
+			d.m(x);
+		}
 	}
-	
+
 	public void n2() {
 		D d = new D();
 		d.m(5);
 	}
-	
+
 	public static void main(String[] args) {
 		C c = new C();
-		c.n2();
+		c.n(null);		
 	}
 }
