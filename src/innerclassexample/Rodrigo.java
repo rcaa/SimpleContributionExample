c46d167f2d9e77266974ef099bfb54a285bc9cbe
Rodrigo Andrade <rodrigo_cardoso@hotmail.it>
2015-11-24 22:06:34 -0300

    inner class example

---
+++ src/innerclassexample/Rodrigo.java
@@ -0,0 +1,22 @@
+package innerclassexample;
+
+public class Rodrigo {
+
+	private String password;
+
+//	public void createInnerClass() {
+//		InnerClass inClass = new InnerClass();
+//		inClass.accessOuter();
+//	}
+
+	class InnerClass {
+
+		public void accessOuter() {
+			System.out.println("The outer class's password is " + password);
+		}
+	}
+	
+	public static void main(String[] args) {
+		Rodrigo r = new Rodrigo();
+	}
+}
