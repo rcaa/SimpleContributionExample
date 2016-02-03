8ba3ff7e37f56e2fadfbd4107f1ca428df5e25b3
Rodrigo Andrade <rodrigo_cardoso@hotmail.it>
2016-02-03 11:22:47 -0300

    inheritance example

---
+++ src/inheritanceexample/ClasseAbstrata.java
@@ -0,0 +1,10 @@
+package inheritanceexample;
+
+
+public abstract class ClasseAbstrata {
+
+	public void doPost() throws Exception {
+
+		throw new Exception();
+	};
+}
---
+++ src/inheritanceexample/ClasseConcreta.java
@@ -0,0 +1,9 @@
+package inheritanceexample;
+
+public class ClasseConcreta extends ClasseAbstrata {
+	
+	@Override
+	public void doPost() throws Exception {
+		System.out.println("testando");
+	}
+}
